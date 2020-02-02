0d8205caf832fcaf4b2d650a99275e99fd165dbc
Ege Uz <enkomaun@gmail.com>
2020-02-02 14:59:01 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles.css';
+import './ege.json';
+
+//JSON
+// const data = require('./ege.json')
+
+function App() {
+
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <p>hello???</p>
+    </div>
+  )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
